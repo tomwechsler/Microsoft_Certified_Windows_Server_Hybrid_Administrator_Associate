9c9222131741fdb58d855535ea3b8e9b01ac5f54
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2022-04-09 06:51:31 +0200

    Update 00_Basic_Cluster_Lab_Setup.ps1

--- AZ-801/00_Basic_Cluster_Lab_Setup.ps1
+++ AZ-801/00_Basic_Cluster_Lab_Setup.ps1
@@ -3,7 +3,7 @@ New-VMSwitch -SwitchName "192.168.3.0-NATSwitch" -SwitchType Internal
 
 New-NetIPAddress -IPAddress 192.168.3.2 -PrefixLength 24 -InterfaceAlias "vEthernet (192.168.3.0-NATSwitch)"
 
-New-NetNAT -Name "192.168.0.3-NATNetwork" -InternalIPInterfaceAddressPrefix 192.168.0.3/24
+New-NetNAT -Name "192.168.3.0-NATNetwork" -InternalIPInterfaceAddressPrefix 192.168.3.0/24
 
 #On all virtual systems you want to manage (absolutely fine for the test environment - never adjust the firewall like this in a prductive environment)
 Enable-PSRemoting
