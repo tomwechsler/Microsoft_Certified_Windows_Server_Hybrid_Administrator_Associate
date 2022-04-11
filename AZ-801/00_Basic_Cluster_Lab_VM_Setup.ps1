4ca206045a6d6a474eea119e0362a9cf18da52f1
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2022-04-11 06:53:38 +0200

    Add files via upload

---
+++ AZ-801/00_Basic_Cluster_Lab_VM_Setup.ps1
@@ -0,0 +1,16 @@
+﻿Get-NetIPInterface
+
+Set-NetIPInterface -InterfaceIndex "Ethernet" -Dhcp Disabled
+
+New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress 192.168.3.20 -PrefixLength 24 -DefaultGateway 192.168.3.2
+
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses ("192.168.3.10")
+
+Rename-Computer -NewName clus01 -Restart
+
+$domain = "prime.pri"
+$password = "P@ssw0rd" | ConvertTo-SecureString -asPlainText -Force
+$username = "$domain\administrator" 
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+
+Add-Computer -DomainName $domain -Credential $credential -Restart -Force
