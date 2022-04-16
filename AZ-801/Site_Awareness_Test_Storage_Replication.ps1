8cfa88f689076d5b01094805657ff18b26f80a7c
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2022-04-16 16:12:19 +0200

    Add files via upload

---
+++ AZ-801/Site_Awareness_Test_Storage_Replication.ps1
@@ -0,0 +1,22 @@
+﻿Enter-PSSession -ComputerName clus01
+
+#Example: New-ClusterFaultDomain -Type Rack -Name "Rack1" -FaultDomain "Site001"
+
+New-ClusterFaultDomain -Name SiteA -FaultDomainType Site -Description "SiteA" -Location "ZRH"
+New-ClusterFaultDomain -Name SiteA -FaultDomainType Site -Description "SiteB" -Location "BSL"
+Set-ClusterFaultDomain -Name clus01 -Parent "SiteA"
+Set-ClusterFaultDomain -Name clus02 -Parent "SiteA"
+Set-ClusterFaultDomain -Name sitebclus01 -Parent "SiteB"
+Set-ClusterFaultDomain -Name sitebclus02 -Parent "SiteB"
+
+(Get-Cluster).PreferredSite="SiteA"
+
+#On clus01
+Test-SRTopology -SourceComputerName clus01 `
+-SourceVolumeName C:\ClusterStorage\Volume1 `
+-SourceLogVolumeName L: `
+-DestinationComputerName sitebclus01 `
+-DestinationVolumeName M: `
+-DestinationLogVolumeName N: `
+-DurationInMinutes 1 `
+-IgnorePerfTests -ResultPath C:\
