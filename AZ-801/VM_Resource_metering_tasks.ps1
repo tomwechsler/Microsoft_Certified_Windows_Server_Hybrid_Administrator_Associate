85d32f9694c053388f3b883aa9dc137a83e4d8d7
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2022-04-28 06:48:50 +0200

    Add files via upload

---
+++ AZ-801/VM_Resource_metering_tasks.ps1
@@ -0,0 +1,13 @@
+﻿#VM Resource metering tasks
+
+#This cmdlet starts collecting data on a per-VM basis.
+Enable-VMResourceMetering
+
+#This cmdlet disables resource metering on a per-VM basis.
+Disable-VMResourceMetering
+
+#This cmdlet resets VM resource-metering counters.
+Reset-VMResourceMetering 
+
+#This cmdlet displays resource-metering statistics for a specific VM.
+Measure-VM 
