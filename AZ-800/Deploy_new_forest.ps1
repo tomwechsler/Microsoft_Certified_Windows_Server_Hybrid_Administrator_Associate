130855041a75573250f6def7c562c4b546fa4b89
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2022-05-15 07:05:36 +0200

    Add files via upload

---
+++ AZ-800/Deploy_new_forest.ps1
@@ -0,0 +1,20 @@
+﻿#Deploy the first domain controller in a new forest
+
+#Install the server role
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+#Create forest root domain
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "prime.pri" `
+-DomainNetbiosName "PRIME" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+  -SafeModeAdministratorPassword (ConvertTo-SecureString 'P@ssw0rd' -AsPlainText -Force)
+-Force:$true
