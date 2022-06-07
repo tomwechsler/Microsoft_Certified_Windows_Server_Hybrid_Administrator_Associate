5670e675e5d4e295c32b4d3e2b2e7f7bfb193258
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2022-06-07 06:24:27 +0200

    Add files via upload

---
+++ AZ-801/Setup_Microsoft_LAPS.ps1
@@ -0,0 +1,28 @@
+﻿Set-Location C:\
+Clear-Host
+
+#Downloading LAPS
+https://www.microsoft.com/en-us/download/details.aspx?id=46899
+
+#Extend the AD Schema
+Import-module AdmPwd.PS
+Update-AdmPwdADSchema
+
+#Change Computer object permissions
+Set-AdmPwdComputerSelfPermission -OrgUnit LAPSCLIENT
+
+#Assign permissions to the group for password access
+#The extended permissions are only applied to the Domain Admins group
+Find-AdmPwdExtendedRights -Identity "LAPSCLIENT"
+
+#We need to grant the same permissions to “ITAdmins” Security group
+Set-AdmPwdReadPasswordPermission -Identity "LAPSCLIENT" -AllowedPrincipals "ITAdmins"
+
+#We can verify now the permissions
+Find-AdmPwdExtendedRights -Identity "LAPSCLIENT" | Format-List
+
+#Install CSE in Computers
+#Create GPO for LAPS settings
+
+#Checking local administrator password
+Get-AdmPwdPassword -ComputerName SRV01
