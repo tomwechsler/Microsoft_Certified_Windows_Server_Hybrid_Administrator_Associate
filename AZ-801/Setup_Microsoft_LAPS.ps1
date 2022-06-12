bd55a6c97b42d578bdc93d9368d7cb61fecc7b70
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2022-06-12 16:01:06 +0200

    Update Setup_Microsoft_LAPS.ps1

--- AZ-801/Setup_Microsoft_LAPS.ps1
+++ AZ-801/Setup_Microsoft_LAPS.ps1
@@ -9,7 +9,7 @@ Import-module AdmPwd.PS
 Update-AdmPwdADSchema
 
 #Change Computer object permissions
-Set-AdmPwdComputerSelfPermission -OrgUnit LAPSCLIENT
+Set-AdmPwdComputerSelfPermission -OrgUnit "LAPSCLIENT"
 
 #Assign permissions to the group for password access
 #The extended permissions are only applied to the Domain Admins group
@@ -19,7 +19,7 @@ Find-AdmPwdExtendedRights -Identity "LAPSCLIENT"
 Set-AdmPwdReadPasswordPermission -Identity "LAPSCLIENT" -AllowedPrincipals prime\Administrator,prime\ITAdmins
 
 #We need to grant the write permissions to "ITAdmins" Security group (The administrators optionally also added)
-Set-AdmPwdResetPasswordPermission -OrgUnit Client -AllowedPrincipals prime\Administrator,prime\ITAdmins
+Set-AdmPwdResetPasswordPermission -OrgUnit "LAPSCLIENT" -AllowedPrincipals prime\Administrator,prime\ITAdmins
 
 #We can verify now the permissions
 Find-AdmPwdExtendedRights -Identity "LAPSCLIENT" | Format-List
