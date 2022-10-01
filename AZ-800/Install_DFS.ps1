f3dd376009fa33d553df3e0a92c0643232a05b60
Tom Wechsler [Microsoft Azure & CDM MVP] <56835948+tomwechsler@users.noreply.github.com>
2022-10-01 07:02:33 +0200

    Add files via upload

---
+++ AZ-800/Install_DFS.ps1
@@ -0,0 +1,8 @@
+﻿Set-Location C:\
+Clear-Host
+
+#Search for the feature
+Invoke-Command -ComputerName srv01,file01 -ScriptBlock {Get-WindowsFeature -Name *dfs-*}
+
+#Install DFS
+Invoke-Command -ComputerName srv01,file01 -ScriptBlock {Install-WindowsFeature -Name FS-DFS-Namespace,FS-DFS-Replication -IncludeManagementTools -IncludeAllSubFeature}
